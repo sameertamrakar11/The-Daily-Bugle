3c92c9f5512e2d2fc376e580a9f61e0046156b41
sameertamrakar11 <sameertamrakar11@outlook.com>
2024-01-04 18:07:52 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = () => {
             progress={progress}
           />
           <Switch>
-            <Route exact path="/"> <News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" /></Route>
+            <Route exact path="/The-Daily-Bugle"> <News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" /></Route>
             <Route exact path="/business"> <News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business" /></Route>
             <Route exact path="/entertainment"> <News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment" /></Route>
             <Route exact path="/general"> <News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" /></Route>
